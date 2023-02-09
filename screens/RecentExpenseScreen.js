7ee18d42a1d9c25c90bb0ca98cd70ffda2a53536
Attariq <attgan@gmail.com>
2023-02-09 08:40:49 +0700

    add recent expense filter

--- screens/RecentExpenseScreen.js
+++ screens/RecentExpenseScreen.js
@@ -2,11 +2,19 @@ import { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ExpenseOutput from "../components/ExpenseOutput/ExpenseOutput";
 import { ExpenseContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../utils/date";
 
 function RecentExpense() {
   const { expenses } = useContext(ExpenseContext);
 
-  return <ExpenseOutput expenses={expenses} periodName={"Last 7 days"} />;
+  const recentExpenses = expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date > date7DaysAgo;
+  });
+
+  return <ExpenseOutput expenses={recentExpenses} periodName={"Last 7 days"} />;
 }
 
 export default RecentExpense;
