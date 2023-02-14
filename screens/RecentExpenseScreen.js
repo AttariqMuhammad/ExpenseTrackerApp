b65646d222fcd1311824b013a8a189e531c3bdaa
Attariq <attgan@gmail.com>
2023-02-14 10:33:57 +0700

    add laoding case on load, update, delete data from firebase

--- screens/RecentExpenseScreen.js
+++ screens/RecentExpenseScreen.js
@@ -1,21 +1,32 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import ExpenseOutput from "../components/ExpenseOutput/ExpenseOutput";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { ExpenseContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../utils/date";
 import { fetchExpense } from "../utils/https";
 
 function RecentExpense() {
+  //loading useState
+  const [isFetching, setIsFetching] = useState(true);
+
+  //context
   const expenseCtx = useContext(ExpenseContext);
 
   useEffect(() => {
     async function getExpense() {
+      setIsFetching(true);
       const expenses = await fetchExpense();
+      setIsFetching(false);
       expenseCtx.setExpense(expenses);
     }
 
     getExpense();
   }, []);
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
 
   const recentExpenses = expenseCtx.expenses.filter((expense) => {
     const today = new Date();
